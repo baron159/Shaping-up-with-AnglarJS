80b5edd7121833c2a529dca1c5943d9a64421658
Scott Baron <seb8653@rit.edu>
2016-01-15 12:21:21 -0500

    Hello Scott

--- First_Time/modu.js
+++ First_Time/modu.js
@@ -1,6 +1,6 @@
 
 (function () {
-    var app = angular.module('store', []);
+    var app = angular.module('GemStore', []);
     app.controller('StoreController', function(){
         this.products = gems;
     });
@@ -8,8 +8,18 @@
     var gems = [
         {
             name: 'Scottgemstone',
-            price: 3.95,
+            price: 3,
             description: 'Best Gem in the whole world',
+            images: [
+                {
+                    full: 'img/full/m1.jpg',
+                    thumb: 'img/thumbs/t1.jpg'
+                },
+                {
+                    full: 'img/full/m2.jpg',
+                    thumb: 'img/thumbs/t2.jpg'
+                }
+            ],
             canPurchase:false,
             soldOut:false,
         },
@@ -17,6 +27,16 @@
             name: 'ritgemstone',
             price: 44000.95,
             description: 'We hope its worth that much',
+            images: [
+                {
+                    full: 'img/full/m2.jpg',
+                    thumb: 'img/thumbs/t2.jpg'
+                },
+                {
+                    full: 'img/full/m4.jpg',
+                    thumb: 'img/thumbs/t4.jpg'
+                }
+            ],
             canPurchase:true,
             soldOut:false,
         },
@@ -24,6 +44,16 @@
             name: 'Ryangemstone',
             price: 2.95,
             description: 'Best Gem in the whole world',
+            images: [
+                {
+                    full: 'img/full/m4.jpg',
+                    thumb: 'img/thumbs/t4.jpg'
+                },
+                {
+                    full: 'img/full/m3.jpg',
+                    thumb: 'img/thumbs/t3.jpg'
+                }
+            ],
             canPurchase:false,
             soldOut:false,
         },
@@ -31,6 +61,16 @@
             name: 'Barongemstone',
             price: 8.95,
             description: 'Best Gem in the whole world',
+            images: [
+                {
+                    full: 'img/full/m3.jpg',
+                    thumb: 'img/thumbs/t3.jpg'
+                },
+                {
+                    full: 'img/full/m1.jpg',
+                    thumb: 'img/thumbs/t1.jpg'
+                }
+            ],
             canPurchase:true,
             soldOut:true,
         }
